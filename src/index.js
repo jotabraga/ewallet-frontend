7da555e0ee80cd48391ec04ba1d049a202282eac
jotabraga <jpmbraga.feg@gmail.com>
2021-06-22 11:53:02 -0300

    Config commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector(".root"))
+
+//font-family: 'Raleway', sans-serif;
+//font-family: 'Saira Stencil One', cursive;
