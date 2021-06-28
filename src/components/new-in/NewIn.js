1ff25525f457f387e071f76c72f301987da7735d
jotabraga <jpmbraga.feg@gmail.com>
2021-06-28 16:35:35 -0300

    Value bug correction

--- src/components/new-in/NewIn.js
+++ src/components/new-in/NewIn.js
@@ -1,27 +1,66 @@
 import React, { useState, useContext } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { BiArrowBack } from 'react-icons/bi';
 import MainContainer from "../standard-frameworks/MainContainer";
 import NameContainer from "../standard-frameworks/NameContainer";
 import FormContainer from "../standard-frameworks/FormContainer";
+import UserContext from "../UserContext";
+import axios from "axios";
 
-export default function MainPage() {
-  const [value, setValue] = useState(null);
+export default function NewIn() {
+  const [amount, setAmount] = useState(null);
+  const [disabled, setDisabled] = useState(false);
   const [description, setDescription] = useState("");
+  const {user} = useContext(UserContext);
+  const history = useHistory();
+
+  function incomeRegister(e){
+
+    e.preventDefault();
+		setDisabled(true);
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${user.token}`,
+			},
+		};
+
+		const data = {
+			amount,
+			description,
+      type: 'deposit'
+		};
+
+    console.log(data);
+
+		const request = axios.post('http://localhost:4000/transactions', data, config);
+		request.then((res) => {
+				history.push('/main');
+			})
+		request.catch((e) => {
+				console.log(e);
+			});
+		setDisabled(false);
+
+  }
 
   return (
     <MainContainer>
-      <NameContainer justify="left">
+      <NameContainer justify="space-between">
         <span>Nova entrada</span>
+        <Link to="/main">
+					<BiArrowBack size="26px" color="white" />
+				</Link>
       </NameContainer>
 
       <FormContainer position="start" top="40px">
-        <form>
+        <form disabled={disabled} onSubmit={incomeRegister}>
           <input
             type="number"
             required
             placeholder="Valor"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
           />
 
           <input
--- src/components/new-out/NewOut.js
+++ src/components/new-out/NewOut.js
@@ -3,10 +3,46 @@ import { useHistory } from "react-router-dom";
 import MainContainer from "../standard-frameworks/MainContainer";
 import NameContainer from "../standard-frameworks/NameContainer";
 import FormContainer from "../standard-frameworks/FormContainer";
+import UserContext from "../UserContext";
+import axios from "axios";
 
-export default function MainPage() {
-  const [value, setValue] = useState(null);
+export default function NewOut() {
+
+  const [amount, setAmount] = useState(null);
+  const [disabled, setDisabled] = useState(false);
   const [description, setDescription] = useState("");
+  const {user} = useContext(UserContext);
+  const history = useHistory();
+
+  function expenseRegister(e){
+
+    e.preventDefault();
+		setDisabled(true);
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${user.token}`,
+			},
+		};
+
+		const data = {
+			amount,
+			description,
+      type: 'expense'
+		};
+
+    console.log(data);
+
+		const request = axios.post('http://localhost:4000/transactions', data, config);
+		request.then((res) => {
+				history.push('/main');
+			})
+		request.catch((e) => {
+				console.log(e);
+			});
+		setDisabled(false);
+
+  }
 
   return (
     <MainContainer>
@@ -15,13 +51,13 @@ export default function MainPage() {
       </NameContainer>
 
       <FormContainer position="start" top="40px">
-        <form>
+        <form disabled={disabled} onSubmit={expenseRegister} >
           <input
             type="number"
             required
             placeholder="Valor"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
           />
 
           <input
